12a5d4184b08dbc832c28b1778e802e9534d1cb0
GabrielNew <gabrielfai70@gmail.com>
2025-01-06 14:35:31 -0300

    Mean and variation info

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+const fs = require("fs");
 const request = require("postman-request");
 
 const url = `${process.env.API_URL}eod?access_key=${process.env.API_KEY}&symbols=MSFT`;
 
-//console.log(url);
+request({ url: url, json: true }, (error, response) => {
+  let sumValue = 0;
+  let perVariation = 0;
+  response.body.data.forEach((element) => {
+    sumValue += element.close;
+  });
+  perVariation =
+    ((response.body.data[0].close - response.body.data[99].close) /
+      response.body.data[99].close) *
+    100;
 
-request({ url: url }, (error, response) => {
-  console.log(response);
+  console.log(
+    "Mean value in the last 100 days: " + (sumValue / 100).toFixed(3)
+  );
+  console.log(
+    "The stock price has changed by " +
+      perVariation.toFixed(2) +
+      "% over the last 100 days."
+  );
+
+  //fs.writeFileSync("example.json", JSON.stringify(response.body));
 });
+
+//const data = JSON.parse(fs.readFileSync("example.json"));
